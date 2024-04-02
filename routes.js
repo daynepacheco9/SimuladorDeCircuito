90564dc0ce3732c99136b5f819aa0f70c089d28e
Dayne Pacheco <daynepacheco@gmail.com>
2024-04-02 15:08:03 -0300

    Arrumando tela de cadastro

--- routes.js
+++ routes.js
@@ -6,11 +6,12 @@ const home = require('./src/controllers/home');
 
 // Iniciando as rotas
 route.get('/', home.pagInicialGet);
-
-
-
-
-
+route.get('/pagina-cad-comp', home.pagCadCompGet);
+route.get('/pagina-cadastro', home.pagCadastroGet);
+route.get('/pagina-login', home.pagLoginGet);
+route.get('/pagina-principal', home.pagPrincipalGet);
+route.get('/pagina-senha', home.pagSenhaGet);
+route.get('/pagina-simulador', home.pagSimuladorGet);
 
 
 
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -4,5 +4,24 @@
 module.exports = {
     async pagInicialGet(req, res){
     res.render('../views/index');
+    },
+    async pagCadCompGet(req, res){
+        res.render('../views/pagina-cad-comp');
+    },
+    async pagCadastroGet(req, res){
+        res.render('../views/pagina-cadastro');
+    },
+    async pagLoginGet(req, res){
+        res.render('../views/pagina-login');
+    },
+    async pagPrincipalGet(req, res){
+        res.render('../views/pagina-principal');
+    },
+    async pagSenhaGet(req, res){
+        res.render('../views/pagina-senha');
+    },
+    async pagSimuladorGet(req, res){
+        res.render('../views/pagina-simulador');
     }
+    
 }
