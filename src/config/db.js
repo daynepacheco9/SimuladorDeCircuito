314e5ee328d171665d9a2dc4f93e3151667a311a
Dayne <daynepacheco@gmail.com>
2024-04-11 10:58:52 -0300

    re add

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 //configurações da base de dados
 const database = new sequelize('ProjetoJesDay', 'ProjetoJesDay', 'jesday',
 {
-    dialect: 'mssql', host:'localhost', port: 58225
+    dialect: 'mssql', host:'localhost', port: 54582
 });
 database.sync();
 module.exports = database;
